3b7878f51f1ef89b75c5fb89bf88ce27a5b7d059
dldudgjs31 <dldudgjs31@naver.com>
2022-02-21 13:44:30 +0900

    test

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -5,10 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    public BCryptPasswordEncoder encode(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/cos/photogramstart/service/AuthService.java
+++ src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -3,8 +3,9 @@ package com.cos.photogramstart.service;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,11 +15,18 @@ public class AuthService {
 
 
     private final UserRepository userRepository;
-    
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     
+    @Transactional // write (insert,update,delete 시)
     public User signup(User user){
         //회원가입 진행
+        //패스워드 암호화 진행
+        String rawPassword=user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER"); // 관리자는 ROLE_ADMIN
+
         User userEntity = userRepository.save(user);
         return userEntity;
     }
